44c9e6269ecd1c12377d5d402088e666a25251f7
arsoolo <erkobola@gmail.com>
2026-03-22 18:47:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import logging
 from aiogram import Bot, Dispatcher, types, executor
 
+# Вставь сюда свой токен от BotFather
 TOKEN = "8756906168:AAEKFWK8SLeG_3fdWB-zgTiYP_IQnMpdFzE"
 
+# Настройка логирования, чтобы видеть ошибки в консоли
 logging.basicConfig(level=logging.INFO)
+
+# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# Ответ на команду /start
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Системы Джарвиса активированы, сэр!")
+async def process_start_command(message: types.Message):
+    await message.reply("Системы Джарвиса активированы. Добрый день, сэр!")
 
+# Реакция на текст
 @dp.message_handler()
-async def echo(message: types.Message):
-    if "джарвис" in message.text.lower():
-        await message.reply("К вашим услугам, сэр.")
+async def echo_message(message: types.Message):
+    text = message.text.lower()
+    
+    if "джарвис" in text:
+        await message.reply("К вашим услугам, сэр. Чем могу помочь?")
+    elif "тони" in text:
+        await message.reply("Я здесь, сэр. Мистер Старк просил передать, что он занят в мастерской.")
 
+# Запуск бота
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
